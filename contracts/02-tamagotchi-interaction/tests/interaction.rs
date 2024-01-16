65b69a315fff0b3300bec33dd4bb16fe5e430d0c
Heriels-Sun <109306893+Heriels-Sun@users.noreply.github.com>
2024-01-15 21:11:57 -0600

    Add files via upload

--- contracts/02-tamagotchi-interaction/tests/interaction.rs
+++ contracts/02-tamagotchi-interaction/tests/interaction.rs
@@ -1,12 +1,52 @@
-use gtest::{Program, System};
-
-// TODO: 0️⃣ Copy tests from the previous lesson and push changes to the master branch
+use gtest::{Program, System, Log};
+use tamagotchi_io::*;
 
 #[test]
-fn interaction_test() {
+fn smoke_test_01() {
     let sys = System::new();
     sys.init_logger();
     let _program = Program::current(&sys);
 
-    // TODO: 6️⃣ Test new functionality
+    _program.send(2, String::from("Heriel"));
+
+    let _res = _program.send(2, TmgAction::Name);
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(TmgEvent::Name("Heriel".to_string()));
+    assert!(_res.contains(&expected_log));
+
+    let _res   = _program.send(2, TmgAction::Age);
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(TmgEvent::Age(0));
+    assert!(_res.contains(&expected_log));
+
+}
+
+#[test]
+fn smoke_test_02() {
+    let sys = System::new();
+    sys.init_logger();
+    let _program = Program::current(&sys);
+
+    _program.send(2, String::from("Sofia"));
+
+    let _res = _program.send(2, TmgAction::Feed);
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(TmgEvent::Fed);
+    assert!(_res.contains(&expected_log));
+
+    let _res = _program.send(2, TmgAction::Entertain);
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(TmgEvent::Entertained);
+    assert!(_res.contains(&expected_log));
+
+    let _res = _program.send(2, TmgAction::Sleep);
+    let expected_log = Log::builder()
+    .dest(2)
+    .payload(TmgEvent::Slept);
+    assert!(_res.contains(&expected_log));
+
 }
